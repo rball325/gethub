98e9199f741d0692dfe590b52a62e6e900232494
Rick Ball <rdball@gmail.com>
2025-01-09 13:32:32 -0600

    Switched to gen plot on client

--- plot.py
+++ plot.py
@@ -1,46 +1,27 @@
-from flask import Flask, request, send_file, render_template_string
+from flask import Flask, request, send_from_directory, jsonify
 import pandas as pd
-import matplotlib.pyplot as plt
-import io
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template_string('''
-        <h1>CSV Line Graph Generator</h1>
-        <form action="/plot" method="post" enctype="multipart/form-data">
-            <label for="file">Choose a CSV file:</label>
-            <input type="file" id="file" name="file">
-            <input type="submit" value="Generate Line Graph">
-        </form>
-    ''')
+    return send_from_directory('.', 'index.html')
 
-@app.route('/plot', methods=['POST'])
-def plot():
+@app.route('/upload', methods=['POST'])
+def upload():
     if 'file' not in request.files:
-        return 'No file part'
+        return 'No file part', 400
     file = request.files['file']
     if file.filename == '':
-        return 'No selected file'
+        return 'No selected file', 400
     df = pd.read_csv(file)
+    data = df.to_dict(orient='list')
+    return jsonify(data)
 
-    # Assuming the first column is the x-axis and the rest are y-axis
-    x = df.iloc[:, 0]
-    for column in df.columns[1:]:
-        plt.plot(x, df[column], label=column)
-
-    plt.xlabel(df.columns[0])
-    plt.ylabel('Values')
-    plt.title('Line Graph')
-    plt.legend()
-
-    buf = io.BytesIO()
-    plt.savefig(buf, format='png')
-    buf.seek(0)
-    plt.close()
-
-    return send_file(buf, mimetype='image/png')
+@app.route('/<path:path>')
+def static_files(path):
+    return send_from_directory('.', path)
 
 if __name__ == '__main__':
     app.run(debug=True)
